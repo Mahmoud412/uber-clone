5cbb9e5d928300681b1f1463120e84b724c32380
Mahmoud412 <52178976+Mahmoud412@users.noreply.github.com>
2022-10-06 14:26:06 -0700

    fixed reduex bug

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -3,7 +3,12 @@ import React from "react";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "./NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../../redux/navSlice";
+
 const Header = ({ navigation }) => {
+  const dispatch = useDispatch();
+
   return (
     <View style={tw`p-5`}>
       <Image
@@ -11,10 +16,32 @@ const Header = ({ navigation }) => {
         source={{ uri: "http://links.papareact.com/gzs" }}
       />
       <GooglePlacesAutocomplete
-        query={{ key: "AIzaSyBdWXLk2Bg4cuQaA4ywGTL-R0N0S3GlLcQ" }}
         nearbyPlacesAPI="GooglePlacesSearch"
         debounce={400}
+        query={{ key: "AIzaSyBdWXLk2Bg4cuQaA4ywGTL-R0N0S3GlLcQ" }}
         placeholder="Where From"
+        enablePoweredByContainer={false}
+        onPress={(data, details = null) => {
+          dispatch(
+            setOrigin({
+              location: details.geometry.location,
+              description: data.description,
+            })
+          );
+          dispatch(setDestination(null));
+        }}
+        fetchDetails={true}
+        returnKeyType={"search"}
+        styles={{
+          textInput: {
+            fontWeight: "700",
+            fontSize: 18,
+            marginTop: 7,
+          },
+          container: {
+            flex: 0,
+          },
+        }}
       />
       <NavOptions navigation={navigation} />
     </View>
