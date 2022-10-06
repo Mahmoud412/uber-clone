47e105c876450facf71ce52dd27340ae947d7e16
Mahmoud412 <52178976+Mahmoud412@users.noreply.github.com>
2022-10-06 13:39:42 -0700

    added googlePlaces

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -2,7 +2,7 @@ import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "./NavOptions";
-
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 const Header = ({ navigation }) => {
   return (
     <View style={tw`p-5`}>
@@ -10,7 +10,12 @@ const Header = ({ navigation }) => {
         style={styles.image}
         source={{ uri: "http://links.papareact.com/gzs" }}
       />
-
+      <GooglePlacesAutocomplete
+        query={{ key: "AIzaSyBdWXLk2Bg4cuQaA4ywGTL-R0N0S3GlLcQ" }}
+        nearbyPlacesAPI="GooglePlacesSearch"
+        debounce={400}
+        placeholder="Where From"
+      />
       <NavOptions navigation={navigation} />
     </View>
   );
